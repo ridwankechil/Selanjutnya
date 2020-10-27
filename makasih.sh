d1717b2aafbc6e14806664b7dc458ccf2eb981a3
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-10-27 16:50:53 +0700

    Create makasih.sh

---
+++ makasih.sh
@@ -0,0 +1,502 @@
+#!/bin/bash
+
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+
+echo $ku "Selamat Datang Di Bagi² Selanjutnya"
+echo $me "perkenalkan Saya Ridwan Kechil panggil saja WRpy"
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+echo $ku "Siapa Nama Kamu "
+read nick;
+
+clear
+echo $me "Selamat datang" $ku $nick $me "di bagi² selanjutnya"
+echo $ku "harap bersabar sayang"
+echo $me "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo $cy "Subscribe: Ridwan Kechil "
+echo $me "Follow : ridwan58aja"
+echo $bi "Facebook: Ridwan Kechil "
+echo $me "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo $ku "Pilih Nomor" $me "Bagi² Selanjutnya"
+echo $ku "1•Akun Full "
+echo $me "2•Akun Next"
+echo $bi "3•Akun Part 1"
+echo $ku "4•Akun Part 2"
+echo $bi "5•Bagi² Selanjutnya"
+read -p "root@Kode Script > " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+
+echo $ku "Akun Full Yakan"
+echo $me "Like Dan Subscribe dulu=>Untuk Yang Belum Subscribe"
+sleep 3
+echo $ku "membuka YouTube"
+termux-open https://youtu.be/wFBzv_5xXhU
+echo
+echo
+clear
+
+echo "Terimakasih" $nick
+sleep 2
+echo "Kumpulan akun Full"
+sleep 2
+echo $bi "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo $me "Gmail>> login Moonton/Gmail . kalian tingal unband ke akun fb kalian"
+echo $ku "gmail :phongforever@gmail.com
+sandi :Phonglee123456"
+echo $bi "gmail : achmadyulfinar@gmail.com
+Sandi: Sibaduyut00 "
+echo $ku " gmail : catalin.v69@gmail.com
+sandi : T3rm1nat0r "
+echo $me "gmail : alialp756@gmail.com
+sandi : Aligs1219971 "
+echo $cy "vico.boy.ah@gmail.com
+Sandi : Azam3011 "
+echo $ku " Gmail: yakupgunduz61@gmail.com
+Sandi :Yakup0542 "
+echo "maaf tidak saya beri gmail dan sandi..
+saya harap kalian mengerti (gmail : Sandi)
+di putus dengan tanda : titik dua"
+echo "mcpetic@wp.pl:> Kamil1998"
+echo "linkharden@gmail.com:> DuBBleH2"
+"fphansa@gmail.com:> MamaEn2611
+andrei2004@gmail.com:> Passw0rd
+nadiachrnsa@gmail.com:> Kalasan03
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+tas6466@gmail.com:Temara115
+mayke.s.coutinho@gmail.com:> Oquepotoc0
+reci.albi@gmail.com:> Twingo97
+tushkansky@gmail.com:> As170895
+nikita1261@mail.ru:> Irishka1972
+dgibby11@gmail.com:> Parker07
+karol.lewandowski2@gmail.com:> BaSiA1994
+ashroth992@gmail.com:> Raiden92
+srw1412@hotmail.com:> Siriwan5
+efje.021@gmail.com:> Satudua34
+nintendo.kakashi@gmail.com:> Awesome1
+ronnymur09@gmail.com:> Overlord09"
+sleep 2
+echo $ku "maaf cuma segitu dan coba buka yg lainya "
+sh makasih
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+
+echo "maaf bagi bagi akun next tidak tercatat"
+sh makasih
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+
+clear
+echo "Selamat datang di Bagi Bagi Part 1
+mungkin membutuhkan waktu yang lama"
+sleep 1
+echo "langsung aja"
+echo $me "Akun Ff ya Gan"
+sleep 3
+echo $bi "Semoga beruntung"
+echo $pu "subscribe dulu"
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+sleep 2
+echo "terimakasih sudah subscribe"
+echo $pu "Semoga Anda Mendapatkan Akun Yang Anda Cari Cari"
+echo $me "Selamat Mechoba"
+echo $pu "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" 
+echo $bi "gw gak maksa sanati nanty unsubs gpp"
+echo "santai aja "
+echo $pu "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+clear
+echo $bi " makasih sudah Subscribe"
+sleep 2
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+sleep 6
+echo $bi "
+====[ FACEBOOK ACCOUNT ]====
+• Email Facebook :  Putra46@gmail.com
+• Password Facebook : putra123$ooko
+====[ INFO ACCOUNT ]====
+• Nickname : 1680340291
+• Level : Level 73
+• Pernah Elite Pass : Pernah S2"
+sleep 15
+echo $or "•> Facebook ACCOUNT <•
+Email :> Alma Andriyani
+Password :> alma01
+• Level : Level 51+
+• Pernah Elite Pass : Pernah S5"
+sleep 10
+echo bi"
+•> Facebook ACCOUNT <•
+Email :> dinoaku098@gmail.com
+Password :> hayukmabar
+• Level : Level 33
+• Pernah Elite Pass : Pernah S12"
+sleep 30
+echo "Email :> ranganation8@gmail.com
+Password :> elivsayang
+• Level : Level 79
+• Pernah Elite Pass : Pernah S1"
+sleep 17
+echo $pu "
+Email :> maulanarudiansyah2@gmail.com 
+Password :> MaulanaSanti
+• Level : Level 70
+• Pernah Elite Pass : Pernah S4"
+sleep 2
+echo $me "Email :> deviarisyara239@gmail.com
+Password :> SayangElu
+• Level : Level 4
+• Pernah Elite Pass : belum pernah"
+sleep 30
+echo $cy"Email :> radityaadit255@gmail.com
+Password :> Mabarduluboz
+• Level : Level 39
+• Pernah Elite Pass : Pernah S12"
+sleep 40
+echo "Email :> dancokcokdan98@gmail.com
+Password :> RonnyRisma2
+• Level : Level 80
+• Pernah Elite Pass : Pernah S1"
+sleep 1
+echo " maaf akun lainya sudah di pakek oleh orang jadi 
+cukup sampai disini saja wassalamu'alaikum wr wb
+Sy Minta Bantuanya untuk 500 Subscriber "
+sleep 1
+echo " untuk yang belum dapat bisa nunggu sampai 500
+Subscriber (-_-) bukan berarti gw maksa kalian subscribe
+yang iklas aja Subscribe {Cukup Sekian Terimakasih}"
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+
+clear
+echo $me "Subscribe Dulu"
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+echo $ku "tunggu 15 detik"
+echo "lanjut klik y"
+read p;
+sleep 15
+echo $ku "Hello guys slamat datang di AkunFree #2 "
+sleep 3
+echo "dan kali ini akan lebih banyak lagi karna sudah mencapai"
+sleep 2
+echo "200 subscriber terimakaih, jadikan channel ini mencapai 500 subscriber"
+sleep 4
+clear
+sleep 1
+echo $ku "Langsung Ambil aja "
+echo "di bawah sendiri ada akun yang"
+echo "lebih banyak lagi jadi jangan di skip "
+sleep 3
+echo $bi "ini dia akun buat kalian ambil sepuasnya "
+sleep 2
+echo $cy "nlsn_sylvan@yahoo.com.ph:Mrdesigner"
+sleep 3
+echo $bi "u.ozkurt@hotmail.com:Jetzeq7602"
+sleep 3
+echo $ku "jangan di skip "
+echo $me "djcollins94@yahoo.com:Andbased47"
+sleep 2
+echo $bi "shahinnazerian@yahoo.com:KINGs22528"
+sleep 3
+echo $pu "narrow38@gmail.com:Automatic4"
+sleep 8
+echo $ij "jvbs013@gmail.com:12345Jvp"
+sleep 7
+echo $pr "gin_pradana@yahoo.com:Dana9322"
+sleep 3
+echo $me "theatermc@gmail.com:Leon2015"
+sleep 2
+echo $me "Mendownload semuanya"
+echo "10 detik:: Bantu like juga Vidio di bawah ini"
+termux-open https://youtu.be/wFBzv_5xXhU
+sleep 10
+echo "di like dulu bozz"
+termux-open https://youtu.be/wFBzv_5xXhU
+echo $pu " tunggu 20 detik"
+sleep 20
+echo "terimakasih " $nick
+echo "sebagai hadiah ini tambahan akun buat kalian "
+sleep 1
+echo $me "papali.judson@gmail.com:Judson123"
+echo $ku "atomicskysteam@live.fr:69360Ternay"
+sleep 10
+echo "stellmachermirko@outlook.de:HaltdeinMau1"
+echo "golikov.n37@gmail.com:Desirash37"
+echo "yannyann972972@gmail.com:Yannou972"
+echo "luk144222@o2.pl:Spr37ze452"
+echo "dominiksekana@seznam.cz:Likos123"
+echo "dopaspos@gmail.com:7830956Susoms"
+echo "nicologalinato35@gmail.com:Qwerty12345"
+echo "hucincraft@gmail.com:Coco2000"
+echo "richard.john.mulholland@gmail.com:Nimbus19"
+echo "pr_herbst@yahoo.de:Gilian100"
+echo "hufavre@gmail.com:Aragon1995"
+echo "achmadyulfinar@gmail.com:Sibaduyut00"
+echo "uzzy_ptm38@yahoo.com:Cosmina2"
+echo "alialp756@gmail.com:Aligs1219971"
+echo "pureblood_spector@hotmail.com:Dragon3000"
+echo "seethkuuko@gmail.com:Paul1103"
+echo "d_sky89@yahoo.com:5567Wewe"
+echo "jgs5093@gmail.com:Skagen3845"
+echo "SpiriT.exe@wp.pl:Kevin400"
+echo "itchelwiduch@hotmail.com:Mikali1993"
+echo "robbiethompson831@gmail.com:Acbrotherh00d"
+echo "rmandella@hotmail.com:Wadd1eWadd1e"
+echo "345rd@mail.ru:Gashlo1233"
+echo "irhamferdi759@gmail.com:123Kpl098"
+echo "id.solahudin@gmail.com:17Agustus"
+echo "ant.mateus96@gmail.com:Nokiaa96"
+echo "kormi0617@gmail.com:Demeter25"
+echo "shinobi068@gmail.com:A7842115a"
+echo "ainun250496@gmail.com:Gandaria25"
+echo "deny_irwansyah77@yahoo.com:Pemainbasket"
+echo "elpguitar@gmail.com:Iceiceice91"
+echo "ksdmdks@gmail.com:Abc64296429"
+echo "matadoresxd@gmail.com:Mxd468200"
+echo "sonnyjuggle@yahoo.co.id:Sialan123"
+echo "t.mueller-son@live.de:Tobias01234"
+echo "vico.boy.ah@gmail.com:Azam3011"
+echo "taylorjoverton@gmail.com:Barney123"
+echo "juggernog45@gmail.com:Nobleteam23"
+echo "k0s0v.iliya995@gmail.com:Fire20081977"
+echo "phuc.phan168@gmail.com:Vn951321"
+echo "kain.schatten@gmail.com:Darkkain666"
+echo "melissa984@hotmail.es:Cholola984"
+echo "zanxusvk@outlook.fr:Shinryujin11"
+echo "hallkpln@gmail.com:areyou012Aq"
+echo "cpt.valentin@gmail.com:Bubble3tea"
+echo "rubenmiguel45@hotmail.com:Vidiojogos"
+echo "nguyendanh0246@gmail.com:Nguyen123"
+echo "pryocrazy@gmail.com:Meisgood66"
+echo "nlsn_sylvan@yahoo.com.ph:Mrdesigner"
+echo "u.ozkurt@hotmail.com:Jetzeq7602"
+echo "mizhevich04@mail.ru:Qzwsdcvb6523"
+echo "djcollins94@yahoo.com:Andbased47"
+echo "dektharivania@gmail.com:Suwara16"
+echo "gin_pradana@yahoo.com:Dana9322"
+echo "jvbs013@gmail.com:12345Jvp"
+echo "felipe.carvalho12@gmail.com:A3261002a"
+echo "theatermc@gmail.com:Leon2015"
+echo "tushkansky@gmail.com:As170895"
+echo "mcpetic@wp.pl:Kamil1998"
+echo "sven.munk@gmx.net:Affenhirn123"
+echo "cris-4-97@hotmail.com:Mierda23"
+echo "phamminhthanh1907@gmail.com:19071986Pvm"
+echo "nikiforov.leonid@mail.ru:135792468Ylitk"
+echo "winer15@mail.ru:65Pain15"
+echo "srw1412@hotmail.com:Siriwan5"
+echo "kguerrero41603boss@gmail.com:Kalani01"
+echo "papali.judson@gmail.com:Judson123"
+echo "atomicskysteam@live.fr:69360Ternay"
+echo "stellmachermirko@outlook.de:HaltdeinMau1"
+echo "nicologalinato35@gmail.com:Qwerty12345"
+echo "dopaspos@gmail.com:7830956Susoms"
+echo "luk144222@o2.pl:Spr37ze452"
+echo "hucincraft@gmail.com:Coco2000 "
+sleep 2
+echo $or "
+  ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+   🔴 Jangan Lupa Tersenyum🔴
+  ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+  
+═══════════════════════════════════════
+[1] 🔴 Akun Lebih Banyak Lagi🔴
+═══════════════════════════════════════
+[2] 🔴 Cukup Sampai Disini🔴
+═══════════════════════════════════════
+┗────CTRL +c Exit Program"
+echo '\033[35;1m'
+read -p "root@Pilih Nomor > " sayang
+if [ $sayang = 1 ] || [ $sayang = 1 ]
+then
+sleep 2
+echo "di like boss "
+termux-open https://youtu.be/wFBzv_5xXhU
+sleep 12
+echo $bi "felipe.carvalho12@gmail.com:A3261002a"
+sleep 6
+echo $pu "dektharivania@gmail.com:Suwara16"
+sleep 2
+echo $me "tushkansky@gmail.com:As170895"
+sleep 1
+echo $cy "cris-4-97@hotmail.com:Mierda23"
+sleep 10
+echo $ku "mcpetic@wp.pl:Kamil1998"
+sleep 1
+echo $bi "nikiforov.leonid@mail.ru:135792468Ylitk"
+sleep 1
+echo $ku "sven.munk@gmx.net:Affenhirn123"
+sleep 3
+echo $bi "phamminhthanh1907@gmail.com:19071986Pvm"
+sleep 2 
+echo $ku "azhbark@gmail.com:Mobizh4032"
+sleep 9
+echo $me "srw1412@hotmail.com:Siriwan5"
+echo $pu "winer15@mail.ru:65Pain15"
+sleep 2
+echo $bi "kguerrero41603boss@gmail.com:Kalani01"
+sleep 2
+echo "jefrideniawan@gmail.com:Jakarta48"
+fi
+
+if [ $sayang = 2 ] || [ $sayang = 2 ]
+
+then
+
+echo $ku "terimakasih sudah berkunjung"
+
+echo $or "share ke taman kalian buar subscriber bisa mqncapai"
+
+echo $me "500 subscriber kalo uda 500+ akan ada akun S1 lagi "
+
+echo "dan akun mobile legends sultan"
+
+echo $pu "Selamat Mencoba"
+
+echo $ku "terimakasih sudah berkunjung"
+echo $or "share ke taman kalian buar subscriber bisa mqncapai"
+echo $me "500 subscriber kalo uda 500+ akan ada akun S1 lagi "
+echo "dan akun mobile legends sultan"
+echo $pu "Selamat Mencoba"
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+echo "lanjut klik y"
+read p;
+echo "maaf akunya kecampur.. ada akun ml ff
+dan Codm ini acar aku males ngurut in.. kalo saat 500 subscribe
+akan aku beri lebih banyak lagi dan lebih jelas lagi"
+echo $ku "bajhoy.butterfly022@gmail.com:Benize0622
+sakuro.jeka@gmail.com:F3ua84fd
+cgkhao69@gmail.com:Akoguapo321
+antuskaaa@gmail.com:Antto123
+gunawan.ei@gmail.com:Indonesia
+nicologalinato35@gmail.com:Qwerty12345
+seliirawan12@gmail.com:Gapero1996
+ilhansandagang7@gmail.com:Ilhan007
+shadodiss@gmail.com:123qweZXC
+flo-g40@hotmail.fr:Monstars40
+jamespescod@gmail.com:Ss657459764
+adytza2006@gmail.com:Stroescu21
+erzrgaming@gmail.com:Zonefir3
+enji005@gmail.com:Enji0319
+hexbug15@gmail.com:TechnoMan75
+purwito26@gmail.com:Pwt212333
+kguerrero41603boss@gmail.com:Kalani01
+darklink512@hotmail.com:Daggerftw1
+gideonsaputratan@gmail.com:Hosana40
+mizhevich04@mail.ru:Qzwsdcvb6523
+egef123egef@gmail.com:Efeege2000
+mieshanich@mail.ru:Jeka1997
+lsxxdddd@gmail.com:Qwe9003836
+guilherme.dc.lnt@gmail.com:Leonfire9988
+jerry-2000@wp.pl:Daimdaim1
+ariantoari402@gmail.com:Arie940429
+ariantoari402@gmail.com:Arie940429
+matheusmeirelles@hotmail.com.br:741Mmatheus
+xoholow@gmail.com:Micho123
+dian.sahputra@gmail.com:Dian1988
+khimjaike_10@yahoo.com:Klonusy23
+carlvin.tran0@gmail.com:Hellomott0
+mitchelwiduch@hotmail.com:Mikali1993
+epxx95@gmail.com:Angel95
+firdausdawud88@gmail.com:Jokam354
+benjaminrenato@gmail.com:Benja038
+maeliss.arbios@gmail.com:Caline11
+woonmin52@gmail.com:Aa9952443
+javelinthrow1@yahoo.com:Ivanov2116
+aldion.prawira@gmail.com:Chipchup123
+alpayunal7@gmail.com:Aa122000
+snivyxlitty@gmail.com:Bouboule00772
+damian.jedi1@gmail.com:Ilbmta18
+kyeeming@gmail.com:Ming1994
+uzzy_ptm38@yahoo.com:Cosmina2
+willrkeith@yahoo.com:Memememe1
+gingin.rizky.s@gmail.com:Cintaraja130
+balos3@gmail.com:Pkloool3
+atomicskysteam@live.fr:69360Ternay
+felipe.carvalho12@gmail.com:A3261002a
+tkrahling@gmail.com:Kameron16
+mirandaroberto98@gmail.com:Tito0246123
+galuhsuwandhana@gmail.com:Suwandhana1
+tranzistor01@gmail.com:nohtai7EeR
+dlastoria@live.com:Demario3
+joao.claro.ck@gmail.com:IDa0fG0D
+gilangramadhanfajri@gmail.com:Ganteng123
+haryharbeno@gmail.com:Haryhebat1994
+ibnuakil93@gmail.com:Ibnuakil93
+dievins.comp@gmail.com:Et69s1ck
+ksdmdks@gmail.com:Abc64296429
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+l.leonphan@gmail.com:Leon18032004
+cassandra.vasquez95@gmail.com:Casa12345
+nuramrulrihan@yahoo.com.my:Xiiinam9
+usmctran93@gmail.com:Lancelot93
+khalikmf@gmail.com:Jin00000
+dimkas24@gmail.com:Plague24
+yannyann972972@gmail.com:Yannou972
+riceman2442@gmail.com:Lakers2442
+devinaureel@yahoo.com:Nutella00
+bigruggiero@gmail.com:Ruggiero20
+budisj25@gmail.com:Sujono123
+u.ozkurt@hotmail.com:Jetzeq7602
+zxurban@gmail.com:Alexalex1
+fastmiks@gmail.com:Fucku1999
+dfajarkurnia@gmail.com:Bismillah
+azhbark@gmail.com:Mobizh4032
+irfan.has.gembok@gmail.com:Ipank123
+fabianahrens23@gmail.com:Dirtdevil1
+natsusam@gmail.com:Chuck1994
+shinobi068@gmail.com:A7842115a
+karatedom2@hotmail.co.uk:Rosieandsandy3
+id.solahudin@gmail.com:17Agustus
+ikhwanraza76@gmail.com:Kuldin1996
+unaymirta@gmail.com:Uncle347
+mohamedsaaif12@gmail.com:Scofild7566588
+alexjguinan@gmail.com:Apollo180
+hardymargono@gmail.com:17Januari2017
+jaybonbon17@gmail.com:AaaPpp17
+d-31121998@mail.ru:Asxw245gc6
+siegel.sven2000@gmail.com:svensven100V
+phuc.phan168@gmail.com:Vn951321
+pramudya_ti08@yahoo.com:Gauntlet123
+hailjane.barcelon@gmail.com:Ekawforever1
+hufavre@gmail.com:Aragon1995
+azterix2far4game@gmail.com:S4lamander
+bennyagsoymendoza@gmail.com:Benny041206
+luk144222@o2.pl:Spr37ze452
+dektharivania@gmail.com:Suwara16
+zakwinkel@gmail.com:Cyberkitty46
+dianhidayat10.dh@gmail.com:Ppkkntl12
+ched_90@yahoo.com.my:Icats2011
+aviorik@gmail.com:Rjhdfktj123
+lilithsithis561@outlook.fr:Lorient56
+marydeandra@gmail.com:Kenza123
+caleb@tetfam.com:Italia27
+rikudonagashi@gmail.com:Naruto212
+rizka.maharani58@yahoo.com:Putihaja91
+kinagi@gmail.com:Ryokoluva200
+jacquelinehermawan@live.com:Tarakanita25
+timo.tuulis@gmail.com:Tuuleke3
+mgesual2@gmail.com:Weebus5188"
+
+echo "maaf karna gada persiapan tiba tiba udah sampai
+270 Subscriber. jadi ga sempat ngedit dan ngatur"
+echo "kebanyakan akun ml dan ff"
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+fi
